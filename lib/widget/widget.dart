bfcaaf0532b94af2e00091053f31b407440cffc1
minjae-drax <mjeong827@gmail.com>
2025-06-13 17:52:50 +0900

    커밋

--- lib/widget/widget.dart
+++ lib/widget/widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toonflex2/screen/detail_screen.dart';
 
 import '../models/webtoon_model.dart';
@@ -6,10 +7,33 @@ import '../services/service.dart';
 
 class WebtoonWidget extends StatelessWidget {
   final WebtoonModel webtoon;
-  const WebtoonWidget({super.key, required this.webtoon});
+  late SharedPreferences prefs;
+  bool isLiked = false;
+
+  WebtoonWidget({super.key, required this.webtoon});
+
+  
+  void initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList('LikedToons');
+    if (likedToons != null) {
+      if(likedToons.contains(webtoon.id) == true) {
+        isLiked = true;
+      }
+    }
+  }
+  @override
+  void initState() {
+    initPrefs();
+  }
 
   @override
   Widget build(BuildContext context) {
+    initPrefs();
+    print(prefs);
+    print(isLiked);
+    print(webtoon.id);
+    print(webtoon.title);
     return GestureDetector(
       onTap: () {
         ApiService.getWebtoonById(webtoon.id).then((value) {
@@ -55,14 +79,20 @@ class WebtoonWidget extends StatelessWidget {
                     ),
           ),
           const SizedBox(height: 10),
-          Text(
-            webtoon.title,
-            style: const TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.w600,
-              ),
-            ),
+          Row(
+            children: [
+              Text(
+                webtoon.title,
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                  ),
+                ),
+              Icon(isLiked ? Icons.favorite : Icons.favorite_border),
+            ],
+          ),
           ],
+          
         ),
     );
   }
